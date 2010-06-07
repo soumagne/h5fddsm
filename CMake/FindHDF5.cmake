cb5cae6a8153da154bdae5b2d04921f2dc7b1736
John Biddiscombe <biddisco@cscs.ch>
2010-06-07 13:51:09 +0200

    Use HDF5 generated config and MPI_FORTRAN_LIBRARY
    Some customizations to the find HDF5 and MPI cmake scripts so that the build uses the correct
    names for debug/release/etc libraries and correctly picks up libs without hard coded-paths
    (Tested ok on win32)

--- CMake/FindHDF5.cmake
+++ CMake/FindHDF5.cmake
@@ -1,28 +1,24 @@
 #
 # Find the native HDF5 includes and library
+# This module assumes that hdf5-1.8.5 or later has been installed using CMake
+# and we therefore pick up the HDF5-config.cmake which contains all we need
 #
-# HDF5_INCLUDE_DIR - where to find H5public.h, etc.
-# HDF5_LIBRARIES   - List of fully qualified libraries to link against when using hdf5.
+# HDF5_CONFIG_FILE - The file that should be included if HDF5_FOUND is true
 # HDF5_FOUND       - Do not attempt to use hdf5 if "no" or undefined.
 
-FIND_PATH(HDF5_INCLUDE_DIR H5public.h
-  /usr/local/include
-  /usr/include
-  "C:/Program Files/HDF5/include"
+FIND_PATH(HDF5_CONFIG_DIR "HDF5-config.cmake"
+  /usr/local/lib
+  /usr/local/lib64
+  /usr/lib
+  /usr/lib64
+  "C:/Program Files/HDF5/lib"
 )
 
-FIND_LIBRARY(HDF5_LIBRARY 
-  NAMES 
-    hdf5 libhdf5 hdf5_D libhdf5_D
-  PATHS 
-    /usr/local/lib
-    /usr/lib
-    "C:/Program Files/HDF5/lib"
+FIND_PATH(HDF5_INCLUDE_DIR "H5public.h"
+  ${HDF5_INSTALL_DIR}/../include
 )
 
 IF(HDF5_INCLUDE_DIR)
-  IF(HDF5_LIBRARY)
-    SET( HDF5_LIBRARIES ${HDF5_LIBRARY} )
-    SET( HDF5_FOUND "YES" )
-  ENDIF(HDF5_LIBRARY)
+  SET(HDF5_CONFIG_FILE "${HDF5_CONFIG_DIR}/HDF5-config.cmake")
+  SET( HDF5_FOUND "YES" )
 ENDIF(HDF5_INCLUDE_DIR)
