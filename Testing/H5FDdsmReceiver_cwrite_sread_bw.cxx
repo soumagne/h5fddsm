b39eb2e8b8b07aa21ea4d3b62ee0b6340878a192
Jerome Soumagne <soumagne@cscs.ch>
2011-05-06 22:07:15 +0200

    Add a dump after particle read

--- Testing/H5FDdsmReceiver_cwrite_sread_bw.cxx
+++ Testing/H5FDdsmReceiver_cwrite_sread_bw.cxx
@@ -138,6 +138,7 @@ int main(int argc, char *argv[])
         // printf("Trying to read %d * %llu particles\n", nremoteprocs, numParticles);
       }
       TestParticleRead(fullname, dsmManager->GetUpdatePiece(), nremoteprocs*numParticles, comm, dsmManager->GetDSMHandle());
+      dsmManager->H5Dump();
       // Sync here
       MPI_Barrier(comm);
       // Clean up for next step
